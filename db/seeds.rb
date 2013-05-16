a7f9a98b5a14d3e97f343ee54b31e0dadb67096a
Codemis <johnathan@missionaldigerati.com>
2013-05-16 11:08:32 -0700

    Added a seed file with already data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Record.destroy_all
+
+records = YAML.load_file(File.join(Rails.root.to_s, "db", "seeds", "records.yml"))
+records['records'].each do |r|
+  puts r
+  new_record = Record.create({car: r['car'], reason: r['reason'], start_lat: nil, start_location: r['start_location'], start_long: nil, start_odometer: r['start_odometer'], start_use_coords: false, stop_lat: nil, stop_location: r['stop_location'], stop_long: nil, stop_odometer: r['stop_odometer'], stop_use_coords: false})
+	new_record.created_at = Date.parse(r['created_at'].to_s)
+	new_record.save!
+end
